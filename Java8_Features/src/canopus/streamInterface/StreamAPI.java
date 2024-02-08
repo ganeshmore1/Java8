2122a31158a3cf15dcf056c6ffaffa073f6d1990
ganeshmore1 <ganeshjmore97@gmail.com>
2024-02-08 16:40:22 +0530

    Adding Overloading code

--- Java8_Features/src/canopus/streamInterface/StreamAPI.java
+++ Java8_Features/src/canopus/streamInterface/StreamAPI.java
@@ -2,4 +2,22 @@ package canopus.streamInterface;
 
 public class StreamAPI {
 
-}
+	
+		public void m1()
+		{
+			System.out.println(" m1 - method");
+		}
+		public int m2()
+		{
+			System.out.println("m2-method");
+			return 1;
+			
+		}
+		public static void main(String[] args) {
+			StreamAPI api=new StreamAPI();
+			api.m1();
+			api.m2();
+		}
+		
+	}
+
